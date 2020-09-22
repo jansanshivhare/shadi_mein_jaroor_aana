cf9f47df782efc255d8ec71510b3f8c0803cfbaa
jansanshivhare <60268381+jansanshivhare@users.noreply.github.com>
2020-09-22 12:05:19 +0530

    title changed

--- src/Components/Home_service.jsx
+++ src/Components/Home_service.jsx
@@ -12,90 +12,90 @@ const HomeService = () => {
         </div>
         <div className='row py-3'>
           <div className='col-4 py-3'>
-            <div class='card' style={{ width: '18rem;' }}>
-              <img src='...' class='card-img-top' alt='...' />
-              <div class='card-body'>
-                <h5 class='card-title'>Birthday</h5>
-                <p class='card-text'>
+            <div className='card' style={{ width: '18rem' }}>
+              <img src='...' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Birthday</h5>
+                <p className='card-text'>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <NavLink to='#' class='btn btn-primary'>
+                <NavLink to='#' className='btn btn-primary'>
                   Go somewhere
                 </NavLink>
               </div>
             </div>
           </div>
           <div className='col-4 py-3'>
-            <div class='card' style={{ width: '18rem;' }}>
-              <img src='...' class='card-img-top' alt='...' />
-              <div class='card-body'>
-                <h5 class='card-title'>Marry</h5>
-                <p class='card-text'>
+            <div className='card' style={{ width: '18rem' }}>
+              <img src='...' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Marry</h5>
+                <p className='card-text'>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <NavLink to='#' class='btn btn-primary'>
+                <NavLink to='#' className='btn btn-primary'>
                   Go somewhere
                 </NavLink>
               </div>
             </div>
           </div>
           <div className='col-4 py-3'>
-            <div class='card' style={{ width: '18rem;' }}>
-              <img src='...' class='card-img-top' alt='...' />
-              <div class='card-body'>
-                <h5 class='card-title'>Art</h5>
-                <p class='card-text'>
+            <div className='card' style={{ width: '18rem' }}>
+              <img src='...' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Art</h5>
+                <p className='card-text'>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <NavLink to='#' class='btn btn-primary'>
+                <NavLink to='#' className='btn btn-primary'>
                   Go somewhere
                 </NavLink>
               </div>
             </div>
           </div>
           <div className='col-4 py-3'>
-            <div class='card' style={{ width: '18rem;' }}>
-              <img src='...' class='card-img-top' alt='...' />
-              <div class='card-body'>
-                <h5 class='card-title'>Pappu</h5>
-                <p class='card-text'>
+            <div className='card' style={{ width: '18rem' }}>
+              <img src='...' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Pappu</h5>
+                <p className='card-text'>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <NavLink to='#' class='btn btn-primary'>
+                <NavLink to='#' className='btn btn-primary'>
                   Go somewhere
                 </NavLink>
               </div>
             </div>
           </div>
           <div className='col-4 py-3'>
-            <div class='card' style={{ width: '18rem;' }}>
-              <img src='...' class='card-img-top' alt='...' />
-              <div class='card-body'>
-                <h5 class='card-title'>Jansan</h5>
-                <p class='card-text'>
+            <div className='card' style={{ width: '18rem' }}>
+              <img src='...' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Jansan</h5>
+                <p className='card-text'>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <NavLink to='#' class='btn btn-primary'>
+                <NavLink to='#' className='btn btn-primary'>
                   Go somewhere
                 </NavLink>
               </div>
             </div>
           </div>
           <div className='col-4 py-3'>
-            <div class='card' style={{ width: '18rem;' }}>
-              <img src='...' class='card-img-top' alt='...' />
-              <div class='card-body'>
-                <h5 class='card-title'>Mera Naam</h5>
-                <p class='card-text'>
+            <div className='card' style={{ width: '18rem' }}>
+              <img src='...' className='card-img-top' alt='...' />
+              <div className='card-body'>
+                <h5 className='card-title'>Mera Naam</h5>
+                <p className='card-text'>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <NavLink to='#' class='btn btn-primary'>
+                <NavLink to='#' className='btn btn-primary'>
                   Go somewhere
                 </NavLink>
               </div>
