8b226860ff9d37a0b23ae6e9121b4ac492734312
jansanshivhare <60268381+jansanshivhare@users.noreply.github.com>
2020-09-29 23:56:55 +0530

    video embedded 01

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -17,6 +17,28 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className='my-5 text-center'>
+        <iframe
+          className='p-2 text-center'
+          title='maroon_sugar'
+          width='480'
+          height='300'
+          src='https://www.youtube.com/embed/09R8_2nJtjg?start=80'
+          frameborder='0'
+          allow='accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        ></iframe>
+        <iframe
+          className='p-2 text-center'
+          title='cool_meditation_music'
+          width='480'
+          height='300'
+          src='https://www.youtube.com/embed/lCOF9LN_Zxs'
+          frameborder='0'
+          allow='accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowfullscreen
+        ></iframe>
+      </div>
     </>
   );
 };
