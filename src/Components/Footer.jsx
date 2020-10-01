f6d3faf812d722d00922ebfeca6616eee53baef9
jansanshivhare <60268381+jansanshivhare@users.noreply.github.com>
2020-10-01 14:22:33 +0530

    footer-adjusted-1-10

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
   return (
     <>
       <footer className='footer'>
-        <div className='container w-75'>
+        <div className='container-fluid'>
           <div className='footer__option '>
             <div className='row mt-5 mx-auto text-center'>
-              <div className='col-lg-4 col-md-12 footer__option__item'>
+              <div className='col-lg-4 col-md-6 col-12 footer__option__item'>
                 <h5>ABOUT US</h5>
                 <hr />
-                <p>
+                <p className='text-left'>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Accusamus consequatur rem illo quis numquam, minima eos quam
                   eum expedita? Reprehenderit quibusdam perferendis repudiandae
@@ -21,7 +21,7 @@ const Footer = () => {
                   </NavLink>
                 </p>
               </div>
-              <div className='col-lg-4 col-md-6 col-6 footer__option__item'>
+              <div className='col-lg-4 col-md-3 col-6 footer__option__item'>
                 <h5>WHO WE ARE</h5>
                 <hr />
                 <ul>
@@ -47,7 +47,7 @@ const Footer = () => {
                   </li>
                 </ul>
               </div>
-              <div className='col-lg-4 col-md-6 col-6 footer__option__item'>
+              <div className='col-lg-4 col-md-3 col-6 footer__option__item'>
                 <h5>OUR WORK</h5>
                 <hr />
                 <ul>
@@ -75,70 +75,10 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div>
-            <h4 className='text-center mt-3'>Follow us on Social Media</h4>
-            <hr />
-            <div className='row text-center'>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-facebook text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-twitter text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-google text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-linkedin text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-youtube text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-instagram text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-pinterest text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-snapchat-ghost text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-skype text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-android text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-yahoo text-white'
-              ></NavLink>
-              <NavLink
-                to='#'
-                class=' m-auto w-100 h-100 col-lg-1 col-md-2 col-sm-2 col-3 fa fa-reddit text-white'
-              ></NavLink>
-              {/* <NavLink to='#' class='fa fa-dribbble text-white'></NavLink> */}
-              {/* <NavLink to='#' class='fa fa-vimeo text-white'></NavLink> */}
-              {/* <NavLink to='#' class='fa fa-tumblr text-white'></NavLink> */}
-              {/* <NavLink to='#' class='fa fa-vine text-white'></NavLink> */}
-              {/* <NavLink to='#' class='fa fa-foursquare text-white'></NavLink> */}
-              {/* <NavLink to='#' class='fa fa-stumbleupon text-white'></NavLink> */}
-              {/* <NavLink to='#' class='fa fa-flickr text-white'></NavLink> */}
-              {/* <NavLink to='#' class='fa fa-rss text-white'></NavLink> */}
-            </div>
-          </div>
+
           <div className='footer__copyright'>
             <div className='text-center mt-4'>
+              <hr />
               <p className='footer__copyright__text mb-0 pb-3'>
                 Copyright &copy; All rights reserved | This template is made
                 with <i aria-hidden='true'>passion</i> by{' '}
