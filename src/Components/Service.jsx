cf6068c6b53054dedeb9f296c2072ed97207a339
jansanshivhare <60268381+jansanshivhare@users.noreply.github.com>
2020-10-02 11:44:19 +0530

    home-services version2 added

---
+++ src/Components/Service.jsx
@@ -0,0 +1,137 @@
+import React from 'react';
+// import { NavLink } from 'react-router-dom';
+
+const HomeService = () => {
+  return (
+    <>
+      <div className='w-100 home_service'>
+        <h3 className='text-center my-5 mx-auto'>Our Services</h3>
+        <hr />
+        <div className='row text-center mx-auto'>
+          <div className='home_service_card my-3 mx-auto col-lg-4 col-md-6 col-sm-6 col-12'>
+            <div className='home_service_front'>
+              <div className='home_service_inner'></div>
+            </div>
+            <div className='home_service_back'>
+              <div
+                className='home_service_inner text-center'
+                style={{ backgroundImage: 'url(../img/back3.jpg)' }}
+              >
+                <h3 className='my-1 mx-auto my-4'>Wedding Plan</h3>
+                <hr />
+                <p className='text-left my-5 px-3'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima, animi. Explicabo vitae eaque repudiandae adipisci,
+                  sunt deleniti! Veritatis id qui, veniam voluptatum quam
+                  tenetur doloribus nulla dolor vitae! Dignissimos, harum?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='home_service_card my-3 mx-auto col-lg-4 col-md-6 col-sm-6 col-12'>
+            <div className='home_service_front'>
+              <div className='home_service_inner'></div>
+            </div>
+            <div className='home_service_back'>
+              <div
+                className='home_service_inner text-center'
+                style={{ backgroundImage: 'url(../img/back3.jpg)' }}
+              >
+                <h3 className='my-1 mx-auto my-4'>Art Contract</h3>
+                <hr />
+                <p className='text-left my-5 px-3'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima, animi. Explicabo vitae eaque repudiandae adipisci,
+                  sunt deleniti! Veritatis id qui, veniam voluptatum quam
+                  tenetur doloribus nulla dolor vitae! Dignissimos, harum?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='home_service_card my-3 mx-auto col-lg-4 col-md-6 col-sm-6 col-12'>
+            <div className='home_service_front'>
+              <div className='home_service_inner'></div>
+            </div>
+            <div className='home_service_back'>
+              <div
+                className='home_service_inner text-center'
+                style={{ backgroundImage: 'url(../img/back3.jpg)' }}
+              >
+                <h3 className='my-1 mx-auto my-4'>Art Events</h3>
+                <hr />
+                <p className='text-left my-5 px-3'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima, animi. Explicabo vitae eaque repudiandae adipisci,
+                  sunt deleniti! Veritatis id qui, veniam voluptatum quam
+                  tenetur doloribus nulla dolor vitae! Dignissimos, harum?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='home_service_card my-3 mx-auto col-lg-4 col-md-6 col-sm-6 col-12'>
+            <div className='home_service_front'>
+              <div className='home_service_inner'></div>
+            </div>
+            <div className='home_service_back'>
+              <div
+                className='home_service_inner text-center'
+                style={{ backgroundImage: 'url(../img/back3.jpg)' }}
+              >
+                <h3 className='my-1 mx-auto my-4'>Architecture</h3>
+                <hr />
+                <p className='text-left my-5 px-3'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima, animi. Explicabo vitae eaque repudiandae adipisci,
+                  sunt deleniti! Veritatis id qui, veniam voluptatum quam
+                  tenetur doloribus nulla dolor vitae! Dignissimos, harum?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='home_service_card my-3 mx-auto col-lg-4 col-md-6 col-sm-6 col-12'>
+            <div className='home_service_front'>
+              <div className='home_service_inner'></div>
+            </div>
+            <div className='home_service_back'>
+              <div
+                className='home_service_inner text-center'
+                style={{ backgroundImage: 'url(../img/back3.jpg)' }}
+              >
+                <h3 className='my-1 mx-auto my-4'>Ocassions</h3>
+                <hr />
+                <p className='text-left my-5 px-3'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima, animi. Explicabo vitae eaque repudiandae adipisci,
+                  sunt deleniti! Veritatis id qui, veniam voluptatum quam
+                  tenetur doloribus nulla dolor vitae! Dignissimos, harum?
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className='home_service_card my-3 mx-auto col-lg-4 col-md-6 col-sm-6 col-12'>
+            <div className='home_service_front'>
+              <div className='home_service_inner'></div>
+            </div>
+            <div className='home_service_back'>
+              <div
+                className='home_service_inner text-center'
+                style={{ backgroundImage: 'url(../img/back3.jpg)' }}
+              >
+                <h3 className='my-1 mx-auto my-4'>Gifts-Zone</h3>
+                <hr />
+                <p className='text-left my-5 px-3'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Minima, animi. Explicabo vitae eaque repudiandae adipisci,
+                  sunt deleniti! Veritatis id qui, veniam voluptatum quam
+                  tenetur doloribus nulla dolor vitae! Dignissimos, harum?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeService;
