a7c780340dea2924d22164266f2e499a75ac0e94
jansanshivhare <60268381+jansanshivhare@users.noreply.github.com>
2020-10-02 15:46:52 +0530

    Updates

---
+++ precache-manifest.907bb437eea7ca59e584bedb9816e917.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a3a054fe478868a4989a351f4493d4",
+    "url": "/shadi_mein_jaroor_aana/index.html"
+  },
+  {
+    "revision": "cf7c7d8ac852d4f88853",
+    "url": "/shadi_mein_jaroor_aana/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f72c0e85e0aefa933b93",
+    "url": "/shadi_mein_jaroor_aana/static/css/main.e3c11957.chunk.css"
+  },
+  {
+    "revision": "cf7c7d8ac852d4f88853",
+    "url": "/shadi_mein_jaroor_aana/static/js/2.61c72068.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/shadi_mein_jaroor_aana/static/js/2.61c72068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72c0e85e0aefa933b93",
+    "url": "/shadi_mein_jaroor_aana/static/js/main.bb458474.chunk.js"
+  },
+  {
+    "revision": "719df30da67684c31e5a",
+    "url": "/shadi_mein_jaroor_aana/static/js/runtime-main.64775899.js"
+  },
+  {
+    "revision": "5d500bcb6c3eea724ab4aabd496a6e2a",
+    "url": "/shadi_mein_jaroor_aana/static/media/back2.5d500bcb.jpg"
+  },
+  {
+    "revision": "deb5a990aaef90718c03318fc9488c04",
+    "url": "/shadi_mein_jaroor_aana/static/media/back4.deb5a990.jpg"
+  },
+  {
+    "revision": "63a7c3cbabf8e594a3bb7bfea7b31223",
+    "url": "/shadi_mein_jaroor_aana/static/media/contactbg.63a7c3cb.jpg"
+  },
+  {
+    "revision": "8c1bb5c6345ab421e76768543474ffc4",
+    "url": "/shadi_mein_jaroor_aana/static/media/wedd_1.8c1bb5c6.jpg"
+  },
+  {
+    "revision": "b8a8fe56443f361a14431ca0ef2f6b1c",
+    "url": "/shadi_mein_jaroor_aana/static/media/wedd_2.b8a8fe56.jpg"
+  },
+  {
+    "revision": "f30f7e2707a43d19b2f83d7f0a16b1dc",
+    "url": "/shadi_mein_jaroor_aana/static/media/wedd_3.f30f7e27.jpg"
+  }
+]);
